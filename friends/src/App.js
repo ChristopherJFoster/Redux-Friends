8e3dd048ee232571d7770901112a83e075d480fe
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-07 16:37:08 -0500

    Refactored App as a functional component. Redux Friends is class-component-free!

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 
 import PrivateRoute from './components/PrivateRoute';
@@ -7,17 +7,15 @@ import FriendsList from './components/FriendsList';
 import { AddFriendForm } from './components/AddFriendForm';
 import { EditFriendForm } from './components/EditFriendForm';
 
-class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <Route path='/login' component={Login} />
-        <PrivateRoute exact path='/' component={FriendsList} />
-        <PrivateRoute path='/addfriendform' component={AddFriendForm} />
-        <PrivateRoute path='/editfriendform' component={EditFriendForm} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className='app'>
+      <Route path='/login' component={Login} />
+      <PrivateRoute exact path='/' component={FriendsList} />
+      <PrivateRoute path='/addfriendform' component={AddFriendForm} />
+      <PrivateRoute path='/editfriendform' component={EditFriendForm} />
+    </div>
+  );
+};
 
 export default App;
--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -6,10 +6,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
     <Route
       {...rest}
       render={routeProps =>
-        // STEP IV - check to see if user is authed. If yes, render <Component />
-        // else render <Redirect />. Redirect will "redirect" the user to whatever
-        // path is supplied to the "to" prop.
-        localStorage.getItem('token') ? (
+        localStorage.getItem('reduxFriendsToken') ? (
           <Component {...routeProps} />
         ) : (
           <Redirect to='/login' />
